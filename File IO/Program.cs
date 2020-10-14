7338750808de68adb3c7706b7e0d5b8497dfe6d6
Theo Holmgren <theokodar@gmail.com>
2020-10-14 09:11:25 +0200

    Add project files.

---
+++ File IO/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+namespace File_IO
+{
+    class Program
+    {
+        static void WriteFile(string Filename)
+        {
+            File.WriteAllText(Filename, "Det här är sparat även när programmet stängs ned.");
+        }
+        static void ReadFile(string Filename)
+        {
+            Console.WriteLine(File.ReadAllText(Filename));
+        }
+        static void Main(string[] args)
+        {
+            string Filename = @"C:\test\IO.txt";
+            WriteFile(Filename);
+            ReadFile(Filename);
+        }
+    }
+}
